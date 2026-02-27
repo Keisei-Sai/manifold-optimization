1898fef0f6e9c6beb8b376503c812d9203084979
Keisei Sai <tsaiqicheng@gmail.com>
2026-02-27 10:25:46 +0900

    Fix concept diagram layout scaling

--- make_concept_figure.py
+++ make_concept_figure.py
@@ -1,26 +1,45 @@
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(8,4))
+fig, ax = plt.subplots(figsize=(8, 3))
 
 texts = [
     "Unconstrained\nOptimization",
     "Add Constraint",
     "Manifold Geometry",
-    "Different\nConvergence"
+    "Different\nConvergence",
 ]
 
-x = [0,1,2,3]
+x = [0, 2, 4, 6]
+y = 0
 
 for xi, t in zip(x, texts):
-    plt.text(xi, 0.5, t, ha="center", va="center",
-             bbox=dict(boxstyle="round,pad=0.4"))
-
-for i in range(len(x) - 1):
-    plt.annotate(
-        "",
-        xy=(x[i+1] - 0.35, 0.5),     # arrow end
-        xytext=(x[i] + 0.35, 0.5),   # arrow start
-        arrowprops=dict(arrowstyle="->", lw=1.5, mutation_scale=12),
+    ax.text(
+        xi, y, t,
+        ha="center",
+        va="center",
+        fontsize=11,
+        bbox=dict(boxstyle="round,pad=0.5", fc="#E8F2FF", ec="#3A7BD5")
     )
-plt.axis("off")
-plt.savefig("figures/concept_diagram.png", dpi=200, bbox_inches="tight")
+
+for i in range(len(x)-1):
+    ax.annotate(
+        "",
+        xy=(x[i+1]-0.6, y),
+        xytext=(x[i]+0.6, y),
+        arrowprops=dict(
+            arrowstyle="->",
+            lw=1.6,
+            mutation_scale=14
+        ),
+    )
+
+ax.set_xlim(-1, 7)
+ax.set_ylim(-1, 1)
+
+ax.axis("off")
+
+plt.savefig(
+    "figures/concept_diagram.png",
+    dpi=200,
+    bbox_inches="tight"
+)
